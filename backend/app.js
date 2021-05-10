a538e65dc99e381d95d61ce11ab88d0d900b3442
marlon notAR <marlon.aelson.gomes@gmail.com>
2021-05-10 17:55:20 -0300

    ajustando

--- backend/app.js
+++ backend/app.js
@@ -1,55 +1,94 @@
-const PreparaConexaoDB = require('./database/PreparaConexaoDB');
-const ConectarDB = require('./database/ConectarDB');
-const ConverteExcelEmScript = require('./uteis/ConverteExcelEmScript');
-const GerarScriptEmTxt = require('./uteis/GerarScriptEmTxt');
+//const PreparaConexaoDB = require('./database/PreparaConexaoDB');
+//const ConectarDB = require('./database/ConectarDB');
+//const ConverteExcelEmScript = require('./uteis/ConverteExcelEmScript');
+//const GerarScriptEmTxt = require('./uteis/GerarScriptEmTxt');
 const planilha = 'produtos'; //nome da planilha e consequentemente da tabela
 const nome_coluna = 'codigo_produto';
 const gerarScriptEmTxt = true; //se vai querer gerar o script em txt
 const comando = 'insert';
 
-planilha = 'caminho_arquivos'
-
-function getDiretorioArquivosExcel(){
-    diretorioArquivosImportacao = document.querySelector('#diretorio_arquivos');
-    alert(diretorioArquivosImportacao);
+function tipoBanco(param){
+    const tipoBanco = param;
+    return tipoBanco;
 }
 
-function getComando(){
-    comando = document.querySelector('#diretorio_arquivos');
-    alert(diretorioArquivosImportacao);
+function tipoComando(param){
+    const tipoComando = param;
+    return tipoComando;
 }
 
-function importar($param){
+function tipoOperacao(param){
+    const tipoOperacao = param;
+    if(tipoOperacao == 'inserir'){
+        return false;
+    }else{
+        return true;
+    }
+}
 
+function servidor(){
+    const servidor = document.querySelector('#servidor').value;
+    return servidor;
+}
+
+function porta(){
+    const porta = document.querySelector('#porta').value;
+    return porta;
+}
+
+function banco(){
+    const banco = document.querySelector('#banco').value;
+    return banco;
+}
+
+function usuario(){
+    const usuario = document.querySelector('#usuario').value;
+    return usuario;
+}
+
+function senha(){
+    const senha = document.querySelector('#senha').value;
+    return senha;
+}
+
+function diretorioArquivosExcelImportacao(){
+    const diretorioArquivosExcelImportacao = document.querySelector('#diretorio_arquivos');
+    return diretorioArquivosExcelImportacao;
+}
+
+function diretorioSaidaScript(){
+    const diretorioSaidaScript = document.querySelector('#diretorio_saida');
+    return diretorioSaidaScript;
+}
+
+function driveNode(){
+    const driveNode;
+    if(tipoBanco() == "firebird"){
+        driveNode = "node-firebird";
+    }
+    return driveNode;
 }
 
 //conexao do banco de dados onde será inserido os dados
-config1 = {
-    "tipo_banco": "firebird", 
-    "drive_node": "node-firebird",
-    "servidor": "localhost",
-    "porta": "3050",
-    "banco": "C:/AR/BANCO/PLUS/NATAL_DISTRIBUIDORA.FDB",
-    "usuario": "sysdba",
-    "senha": "masterkey"
+config = {
+    "tipo_banco": tipoBanco(), 
+    "drive_node": driveNode(),
+    "servidor": servidor(),
+    "porta": porta(),
+    "banco": banco(),
+    "usuario": usuario(),
+    "senha": senha()
  };
- /*config2 = {
-    "tipo_banco": "mysql", 
-    "drive_node": "mysql",
-    "servidor": "localhost",
-    "porta": "3306",
-    "banco": "sispem",
-    "usuario": "root",
-    "senha": "769SUPORTESEGURO"
- };*/
+ 
 script = ConverteExcelEmScript(planilha, comando, nome_coluna);
+
 script.then((r) => {
-    configuracao = PreparaConexaoDB(config1);
+    configuracao = PreparaConexaoDB(config);
     for(let i = 1; i < r.length; i++){
         ConectarDB(configuracao, r[i]);
     }
 
-    if(gerarScriptEmTxt){
+    if(tipoOperacao()){
         GerarScriptEmTxt(planilha, r);
     }
 });
